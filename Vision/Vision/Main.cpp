a6e4b6dcbe267c3f386f3167aa50c6322cc2bf99
mwerensteijn <mitchellwerensteijn@msn.com>
2014-02-08 17:16:11 +0100

    Grayscale function added
    Grayscale function added. Calculation source:
    http://tech.pro/tutorial/660/csharp-tutorial-convert-a-color-image-to-grayscale

--- Vision/Vision/Main.cpp
+++ Vision/Vision/Main.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 void inverse_image(FIBITMAP * bitmap);
+void grayscale_image(FIBITMAP * bitmap);
 
 int main() {
 	FreeImage_Initialise();
@@ -14,10 +15,10 @@ int main() {
 
 	if (fif != FIF_UNKNOWN) {
 		FIBITMAP * bitmap = FreeImage_Load(fif, filename);
-		
-		inverse_image(bitmap);
 
-		FreeImage_Save(FIF_PNG, bitmap, "filename.png", 0);
+		grayscale_image(bitmap);
+
+		FreeImage_Save(FIF_JPEG, bitmap, "filename.jpeg", 0);
 	}
 	else 
 	{
@@ -35,10 +36,6 @@ void inverse_image(FIBITMAP * bitmap) {
 	const int BITMAP_WIDTH = FreeImage_GetWidth(bitmap);
 	const int BITMAP_HEIGHT = FreeImage_GetHeight(bitmap);
 
-	pixel.rgbRed = 255;
-	pixel.rgbGreen = 0;
-	pixel.rgbBlue = 0;
-
 	for (int y = 0; y < BITMAP_HEIGHT; y++) {
 		for (int x = 0; x < BITMAP_WIDTH; x++) {
 			FreeImage_GetPixelColor(bitmap, x, y, &pixel);
@@ -46,6 +43,27 @@ void inverse_image(FIBITMAP * bitmap) {
 			pixel.rgbRed = ~pixel.rgbRed;
 			pixel.rgbGreen = ~pixel.rgbGreen;
 			pixel.rgbBlue = ~pixel.rgbBlue;
+
+			FreeImage_SetPixelColor(bitmap, x, y, &pixel);
+		}
+	}
+}
+
+// Source: http://tech.pro/tutorial/660/csharp-tutorial-convert-a-color-image-to-grayscale
+void grayscale_image(FIBITMAP * bitmap) {
+	RGBQUAD pixel;
+
+	const int BITMAP_WIDTH = FreeImage_GetWidth(bitmap);
+	const int BITMAP_HEIGHT = FreeImage_GetHeight(bitmap);
+
+	for (int y = 0; y < BITMAP_HEIGHT; y++) {
+		for (int x = 0; x < BITMAP_WIDTH; x++) {
+			FreeImage_GetPixelColor(bitmap, x, y, &pixel);
+
+			int grayscale = (pixel.rgbRed * .3) + (pixel.rgbGreen * .59) + (pixel.rgbBlue * .11);
+			pixel.rgbRed = grayscale;
+			pixel.rgbGreen = grayscale;
+			pixel.rgbBlue = grayscale;
 
 			FreeImage_SetPixelColor(bitmap, x, y, &pixel);
 		}
