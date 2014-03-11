90ef2328d020e2cf231a5cdd8918a926f855f7b0
mwerensteijn <mitchellwerensteijn@msn.com>
2014-03-11 13:35:16 +0100

    Quickselect toegevoegd.

--- Verbeterde versie/Vision/Algorithms/MedianAlgorithm.h
+++ Verbeterde versie/Vision/Algorithms/MedianAlgorithm.h
@@ -1,6 +1,8 @@
 #ifndef MEDIANALGORITHM_H
 #define MEDIANALGORITHM_H
 
+#define SWAP(a,b) temp=(a);(a)=(b);(b)=temp;
+
 #include "VisionAlgorithm.h"
 #include "MaskLooper.h"
 #include <vector>
@@ -56,15 +58,60 @@ public:
 
 	double getMedian(int values[], int maskTotal) {
 		double median;
-		size_t size = maskTotal;
+		/*size_t size = maskTotal;
 		std::sort(values, values + maskTotal);
 		if (size % 2 == 0) {
 			median = (values[size / 2 - 1] + values[size / 2]) / 2;
 		}
 		else {
 			median = values[size / 2];
-		}
+		}*/
+
+		median = quickselect(values, maskTotal, maskTotal / 2);
+
 		return median;
+	}
+
+	int quickselect(int *arr, int n, int k) {
+		unsigned long i, ir, j, l, mid;
+		int a, temp;
+
+		l = 0;
+		ir = n - 1;
+		for (;;) {
+			if (ir <= l + 1) {
+				if (ir == l + 1 && arr[ir] < arr[l]) {
+					SWAP(arr[l], arr[ir]);
+				}
+				return arr[k];
+			}
+			else {
+				mid = (l + ir) >> 1;
+				SWAP(arr[mid], arr[l + 1]);
+				if (arr[l] > arr[ir]) {
+					SWAP(arr[l], arr[ir]);
+				}
+				if (arr[l + 1] > arr[ir]) {
+					SWAP(arr[l + 1], arr[ir]);
+				}
+				if (arr[l] > arr[l + 1]) {
+					SWAP(arr[l], arr[l + 1]);
+				}
+				i = l + 1;
+				j = ir;
+				a = arr[l + 1];
+				for (;;) {
+					do i++; while (arr[i] < a);
+					do j--; while (arr[j] > a);
+					if (j < i) break;
+					SWAP(arr[i], arr[j]);
+				}
+				arr[l + 1] = arr[j];
+				arr[j] = a;
+				if (j >= k) ir = j - 1;
+				if (j <= k) l = i;
+			}
+		}
 	}
 };
 
