a441f8d5051d3b2fffd6b444b119b0583fd6f3f1
Yusuf90 <y.syakur@gmail.com>
2014-03-11 12:35:18 +0100

    MedianAlgorithm done

--- Verbeterde versie/Vision/Algorithms/MaskLooper.h
+++ Verbeterde versie/Vision/Algorithms/MaskLooper.h
@@ -1,35 +1,26 @@
 #ifndef MASKLOOPER_H
 #define MASKLOOPER_H
+#define RED 1
+#define GREEN 2
+#define BLUE 3
 
 #include "../Image.h"
 #include <math.h>
+#include <vector>
+
+using namespace std;
 
 class MaskLooper {
 public:
-	void loopMask(unsigned char * src, unsigned char * dst) {
-
-	}
-
-	int timesToLoop(int maskSize, int image_height, int image_width) {
-		int borderPixels;
+	int getBorderPixels(const int &maskSize) {
+		int borderPixels = 0;
 		if (maskSize % 2 == 1) { //uneven mask (has middle pixel)
 			borderPixels = (maskSize - 1) / 2;
 		}
 		else if (maskSize % 2 == 0) { //even mask (doesnt have middle pixel)
 			borderPixels = maskSize / 2;
 		}
-		int skipPixels;
-		skipPixels = (image_height * borderPixels * 2) + (image_width * borderPixels * 2) - (4 * (pow(borderPixels, 2)));
-		int amountOfTimes;
-		amountOfTimes = (image_height * image_width) - skipPixels;
-
-		for (int h = borderPixels; h < image_height - borderPixels; h++) {
-			for (int w = borderPixels; w < image_width - borderPixels; w++) {
-
-			}
-		}
-
-		return amountOfTimes;
+		return borderPixels;
 	}
 };
 
--- Verbeterde versie/Vision/Algorithms/MedianAlgorithm.h
+++ Verbeterde versie/Vision/Algorithms/MedianAlgorithm.h
@@ -3,19 +3,63 @@
 
 #include "VisionAlgorithm.h"
 #include "MaskLooper.h"
+#include <vector>
+#include <algorithm>
+
+#define MASKSIZE 3
 
 class MedianAlgorithm : public VisionAlgorithm {
 public:
 	void VisionAlgorithm::doAlgorithm(Image & image) {
+		MaskLooper p;
+
+		Image source = image;
+
 		int size = image.getWidth() * image.getHeight() * 3;
+		int height = image.getHeight();
+		int width = image.getWidth();
+		int borderPixels = p.getBorderPixels(MASKSIZE);
 
-		unsigned char * ptr = image.getDataPointer();
-		unsigned char * cpy;
+		unsigned char * src = source.getDataPointer();
+		unsigned char * dst = image.getDataPointer();
 
-		memcpy(cpy, ptr, size);
-		
+		memcpy(dst, src, size);
+		int arrayR[MASKSIZE*MASKSIZE];
+		int arrayG[MASKSIZE*MASKSIZE];
+		int arrayB[MASKSIZE*MASKSIZE];
+		int maskTotal = MASKSIZE*MASKSIZE;
+		int counter = 0;
+		for (int h = borderPixels; h < height - borderPixels; h++) {
+			for (int w = borderPixels; w < width - borderPixels; w++) {
+				for (int newW = w; newW < (w + 3); newW++)
+				{
+					for (int newH = h; newH < (h + 3); newH++)
+					{
+						arrayR[counter] = (int)src[3 * ((newH - 1) * width + (newW - 1))];
+						arrayG[counter] = (int)src[3 * ((newH - 1) * width + (newW - 1)) + 1];
+						arrayB[counter] = (int)src[3 * ((newH - 1) * width + (newW - 1)) + 2];
+						counter++;
+					}
+				}
+				dst[(3 * (h * width + w))] = (char)getMedian(arrayR, maskTotal);
+				dst[(3 * (h * width + w)) + 1] = (char)getMedian(arrayG, maskTotal);
+				dst[(3 * (h * width + w)) + 2] = (char)getMedian(arrayB, maskTotal);
+				counter = 0;
+			}
+		}
+	}
 
-
+	double getMedian(int values[], int maskTotal) {
+		double median;
+		size_t size = maskTotal;
+		std::sort(values, values + maskTotal);
+		if (size % 2 == 0) {
+			median = (values[size / 2 - 1] + values[size / 2]) / 2;
+		}
+		else {
+			median = values[size / 2];
+		}
+		return median;
 	}
 };
 
